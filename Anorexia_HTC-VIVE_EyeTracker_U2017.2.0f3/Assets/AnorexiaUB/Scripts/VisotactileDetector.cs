a1befc08c7f973d3fc6caa01f2afd1d47a179b09
RealitatVirtualUB <realitatvirtualub@gmail.com>
2019-02-11 18:55:39 +0100

    fixing bugs of the visotactil

--- Anorexia_HTC-VIVE_EyeTracker_U2017.2.0f3/Assets/AnorexiaUB/Scripts/VisotactileDetector.cs
+++ Anorexia_HTC-VIVE_EyeTracker_U2017.2.0f3/Assets/AnorexiaUB/Scripts/VisotactileDetector.cs
@@ -7,7 +7,9 @@ public class VisotactileDetector : MonoBehaviour {
     private SteamVR_TrackedObject trackedObj;
     private bool inZone = false;
     int touchingZones = 0;
-    Vector3 initSpherePos = Vector3.zero;
+    public Vector3 initSpherePos = Vector3.zero;
+    //public Vector3 desiredPos = Vector3.zero;
+    public float factor = 0.05f;
 
     private SteamVR_Controller.Device Controller
     {
@@ -17,36 +19,55 @@ public class VisotactileDetector : MonoBehaviour {
     void Awake()
     {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
-        initSpherePos = this.transform.position;
+        //initSpherePos = this.transform.position;
+        //desiredPos = initSpherePos;
+
+
     }
 
+    //void Start()
+    //{
+    //    initSpherePos = this.transform.position;
+    //    desiredPos = initSpherePos;
+    //}
 
     // Update is called once per frame
     void Update () {
-        if (inZone)
+        Vector3 objective = Vector3.zero;
+        objective = initSpherePos - this.transform.localPosition;
+
+        if (!inZone && objective.magnitude > factor)
         {
-            Debug.Log("touching body: " + touchingZones);
+            Debug.Log("ending touching body");
+            this.transform.localPosition += (objective.normalized)/1000;
         }
+        else if (!inZone) this.transform.localPosition = initSpherePos;
+            //objective = objective.normalized;
     }
 
-    void OnCollisionStart(Collision other)
+    void OnCollisionEnter(Collision other)
     {
+        
         touchingZones++;
-        this.transform.localPosition = other.transform.position;
-        Debug.Log("starting collision");
+        //this.transform.localPosition = other.transform.position;
+        inZone = true;
+        Debug.Log("starting collision " + inZone);
     }
 
     void OnCollisionExit(Collision other)
     {
+
+        Debug.Log("ending collision " + inZone + touchingZones + " tag " + other.gameObject.tag);
         touchingZones--;
+
         if (touchingZones <=0)
         {
-            this.transform.localPosition = initSpherePos;
+            //desiredPos = initSpherePos;
             this.GetComponent<Rigidbody>().velocity = Vector3.zero;
             //this.GetComponent<HingeJoint>().constantForce
+            inZone = false;
         }
         //this.transform.localPosition = initSpherePos;
-        Debug.Log("ending collision");
     }
    
 
